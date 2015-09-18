b5ad0ec2dbc8eb91cb3311511e9e496933deed5d
Eric Rogers <EricRogers@Erics-MacBook-Air.local>
2015-09-17 18:26:23 -0700

    adding lesson 5 homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,6 +40,57 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+function getPlayerMove(move) {
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    return move || getInput();
+}
+
+function getComputerMove(move) {
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    return move || randomPlay();
+}
+
+function getWinner(playerMove,computerMove) {
+    var winner;
+    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    /* YOUR CODE HERE */
+if (playerMove === 'rock' && computerMove === 'scissors') {
+    winner = 'player';
+    console.log('you are the winner!');}
+else if (playerMove === 'rock' && computerMove === 'paper') {
+    winner = 'computer';
+    console.log('you lost to the computer!');}
+else if (playerMove === 'rock' && computerMove === 'rock') {
+    winner = 'tie';
+    console.log('it is a tie!');}
+else if (playerMove === 'paper' && computerMove === 'rock') {
+    winner = 'player';
+    console.log('you are the winner!');}
+else if (playerMove === 'paper' && computerMove === 'scissors') {
+    winner = 'computer';
+    console.log('you lost to the computer!');}
+else if (playerMove === 'paper' && computerMove === 'paper') {
+    winner = 'tie';
+    console.log('it is a tie!');}
+else if (playerMove === 'scissors' && computerMove === 'paper') {
+    winner = 'player';
+    console.log('you are the winner!');}
+else if (playerMove === 'scissors' && computerMove === 'rock') {
+    winner = 'computer';
+    console.log('you lost to the computer!');}
+else if (playerMove === 'scissors' && computerMove === 'scissors') {
+    winner = 'tie';
+    console.log('it is a tie!');}
+else {
+    winner = 'error! enter either rock, paper, or scissors'}
+}
+}
+
     return winner;
 }
 
